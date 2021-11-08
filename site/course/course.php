cf851e2e09693d6973f2b98b41794bad329dc57c
soncoiz02 <66171585+soncoiz02@users.noreply.github.com>
2021-11-08 13:14:13 +0700

    done courses page

---
+++ site/course/course.php
@@ -0,0 +1,275 @@
+<div class="courses">
+    <section class="courses__signed">
+        <div class="container">
+            <h2 class="title">Khóa học đã đăng ký</h2>
+            <div class="list-courses">
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="courses__basic">
+        <div class="container">
+            <h2 class="title">Khóa học cơ bản</h2>
+            <div class="list-courses">
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="courses__front-end">
+        <div class="container">
+            <h2 class="title">Khóa học Front-end</h2>
+            <div class="list-courses">
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="courses__back-end">
+        <div class="container">
+            <h2 class="title">Khóa học Back-end</h2>
+            <div class="list-courses">
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+                <div class="course-item">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <a href="" class="name">
+                        Tên khóa học
+                    </a>
+                    <div class="user-count">
+                        <i class="fas fa-users"></i>
+                        <span>12,000</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+</div>
